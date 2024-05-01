9bd09a19ab86a98c6de51c68576e7fed49ad637d
Prince95-cmd <obiekezieprincec@gmail.com>
2024-05-02 00:14:15 +0100

    first commit

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -1,7 +1,23 @@
-function openNav() {
-    document.getElementById("SecondSidebar").style.width = "340px";
-  }
+
+
+window.addEventListener("DOMContentLoaded", () => {
+
+const bars = document.querySelector(".bars");
+const sidebar = document.querySelector(".sidebar1");
+
+bars.addEventListener("click", () => {
+  bars.classList.toggle("active")
+  sidebar.classList.toggle("active");
+})
+
+
+})
+
+// function openNav() {
+//     document.getElementById("SecondSidebar").style.width = "340px";
+//   }
   
-  function closeNav() {
-    document.getElementById("SecondSidebar").style.width = "0";
-  }
+//   function closeNav() {
+//     document.getElementById("SecondSidebar").style.width = "0";
+//   }
+
