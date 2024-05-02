1fd39fbfa4bd5e7e1145f31175770fcbe424df7a
Prince95-cmd <obiekezieprincec@gmail.com>
2024-05-02 03:12:34 +0100

    first commit

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -2,22 +2,27 @@
 
 window.addEventListener("DOMContentLoaded", () => {
 
+  //  Nav Dropdown
 const bars = document.querySelector(".bars");
 const sidebar = document.querySelector(".sidebar1");
+
+// Side nav bar 
+const reveal = document.querySelector(".reveal");
+const slideBack = document.getElementById("close");
 
 bars.addEventListener("click", () => {
   bars.classList.toggle("active")
   sidebar.classList.toggle("active");
 })
 
+reveal.addEventListener("click", () => {
+  document.getElementById("SecondSidebar").style.width = "340px";
+});
+
+slideBack.addEventListener("click", () => {
+  document.getElementById("SecondSidebar").style.width = "0";
+});
+
 
 })
-
-// function openNav() {
-//     document.getElementById("SecondSidebar").style.width = "340px";
-//   }
-  
-//   function closeNav() {
-//     document.getElementById("SecondSidebar").style.width = "0";
-//   }
 
