9de4f5e87a1210004b557baadb7aba7f5ec2e3f9
Prince95-cmd <obiekezieprincec@gmail.com>
2024-05-04 22:48:08 +0100

    first commit

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -23,6 +23,82 @@ slideBack.addEventListener("click", () => {
   document.getElementById("SecondSidebar").style.width = "0";
 });
 
+// Section one
 
+const buttons = document.querySelectorAll('.showDivButton');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const targetId = button.getAttribute('data-target');
+    const targetDiv = document.getElementById(targetId);
+
+    if (targetDiv) {
+      const divs = document.querySelectorAll('.self');
+      divs.forEach(div => {
+        if (div.id === targetId) {
+          div.classList.remove('hidden');
+        } else {
+          div.classList.add('hidden');
+        }
+      });
+    }
+  });
+});
+
+
+
+
+
+
+
+
+// Scroll animation
+function kickStart() {
+  var start = document.querySelectorAll(".kickStart")
+
+  for (let i = 0; i < start.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = start[i].getBoundingClientRect().top;
+    let elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      start[i].classList.add("active");
+    } else {
+      start[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", kickStart);
+kickStart()
+
+
+
+// function revealElement(element){
+//   let reveals = document.querySelectorAll(`${element}`)
+//   for(let i = 0; i<reveals.length; i++){
+//        let windowHeight = window.innerHeight;
+//        let revealTop = reveals[i].getBoundingClientRect().top;
+//        let revealPoint = 150;
+       
+
+//        if(revealTop < windowHeight - revealPoint  ){
+//           reveals[i].classList.add('active');
+     
+//                   //  console.log(reveals[i])
+
+
+//        }else{
+//           reveals[i].classList.remove('active');
+//        }
+//   }
+// }
+
+// window.addEventListener("scroll", ()=>{
+//   revealElement('.revealElement')
+//   reveal('.translateYN')
+//   reveal('.pop')
+//   reveal('.flip')
+// })
 })
 
