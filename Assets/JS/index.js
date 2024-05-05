a76a6670a318710ec6fc16dcfd61ca0bf70d0be5
Prince95-cmd <obiekezieprincec@gmail.com>
2024-05-06 00:57:38 +0100

    first commit

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -43,6 +43,32 @@ kickStart(".translateY")
 kickStart(".translateYN")
 kickStart(".flip")
 kickStart(".pop")
+kickStart(".visible")
+})
+
+// Section 8
+const blogShowcaseContainer = document.querySelectorAll(".blogShowcase");
+const blogPhotos = document.querySelectorAll(".blogPhoto");
+const blogIconsContainer = document.querySelectorAll(".blogFa");
+const blogIconEye = document.querySelectorAll(".fa-eye");
+const headerTwo = document.querySelectorAll(".header2")
+
+blogShowcaseContainer.forEach((blogContainer, i) => {
+  blogContainer.addEventListener("mouseover", () => {
+    blogPhotos[i].style.visibility = "visible"; //Show image
+    blogIconsContainer[i].style.borderColor = "var(--color3)"; // change border color
+    blogIconEye[i].style.color= "var(--color3)"; // change icon color
+    headerTwo[i].style.color = "var(--color3)"; // change h2 color
+  } )
+})
+
+blogShowcaseContainer.forEach((blogContainer, i) => {
+  blogContainer.addEventListener("mouseout", () => {
+    blogPhotos[i].style.visibility = "hidden"; // remove image
+    blogIconsContainer[i].style.borderColor = "initial"; 
+    blogIconEye[i].style.color= "var(--color";
+    headerTwo[i].style.color = "var(--color";
+  } )
 })
 
 })
